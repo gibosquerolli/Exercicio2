f787155b037dfa005c4e6040ec1dd4f2133a0241
camil <camil@Camille>
2025-05-12 19:18:23 -0300

    if, else

---
+++ src/main/java/com/mycompany/exercicio2/Exercicio2.java
@@ -0,0 +1,14 @@
+package com.mycompany.exercicio2;
+
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+        int idade = 17;
+
+        if (idade >= 18) {
+            System.out.println("Pode votar.");
+        } else {
+            System.out.println("Não pode votar.");
+        }
+    }
+}
